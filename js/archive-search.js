34b3de2604c933342897687dad34e4c4f93a4968
Tim Kallmer <tkallmer@gmail.com>
2026-07-27 10:01:34 -0400

    Add shared archive search foundation

--- js/archive-search.js
+++ js/archive-search.js
@@ -2,3 +2,31 @@
  * Archive Search
  * Shared person search component used throughout the archive.
  */
+
+"use strict";
+
+class ArchiveSearch {
+  constructor(options = {}) {
+    this.options = options;
+
+    this.people = [];
+    this.peopleById = new Map();
+    this.searchablePeople = [];
+  }
+
+  setPeople(people = []) {
+    this.people = [...people];
+
+    this.peopleById = new Map(
+      this.people.map((person) => [person.id, person]),
+    );
+
+    this.searchablePeople = [];
+  }
+
+  getPerson(id) {
+    return this.peopleById.get(id) ?? null;
+  }
+}
+
+window.ArchiveSearch = ArchiveSearch;
